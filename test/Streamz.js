59fb1f5feb9367791162c59f2eebd2bb15bc8803
Steve Boyer <stephen.a.boyer@gmail.com>
2022-03-31 14:51:16 -0500

    Test for successful contract deployment

--- test/Streamz.js
+++ test/Streamz.js
@@ -3,3 +3,25 @@ const Streamz = artifacts.require('./Streamz.sol')
 require('chai')
   .use(require('chai-as-promised'))
   .should()
+
+
+contract('Streamz', ([deployer, author]) => {
+  let streamz
+
+  describe('deployment', async () => {
+    it('deploys successfully', async () => {
+      streamz = await Streamz.deployed()
+      const address = await streamz.address
+      assert.notEqual(address, 0x0)
+      assert.notEqual(address, '')
+      assert.notEqual(address, null)
+      assert.notEqual(address, undefined)
+    })
+
+  })
+
+
+
+
+
+})
