d7f987ca9efe0bd22980061905ba5540d0c008cb
Steve Boyer <stephen.a.boyer@gmail.com>
2022-04-02 13:43:16 -0500

    Add uploadVideo() skeleton, mapping and event for uploadVideo()

--- src/contracts/Streamz.sol
+++ src/contracts/Streamz.sol
@@ -1,21 +1,50 @@
 pragma solidity ^0.5.16;
 
+// [X] 1. Model the video
+// [ ] 2. Store the video
+// [ ] 3. Upload video
+// [ ] 4. List videos
+
+
 contract Streamz {
 
   string public name = "Streamz";
+  uint public videoCount= 0;
 
 
   // Mapping (store videos)
-
+  mapping(uint => Video) public videos;
 
   // Struct (model videos)
+  struct Video {
+    uint id;
+    string hash;
+    string title;
+    address uploader;
+  }
 
-  // Event
+
+  // Event (video has been uploaded)
+  event VideoUploaded (
+    uint id,
+    string hash,
+    string title,
+    address uploader
+  );
 
   // Constructor
+  constructor() public {
+
+  }
 
 
-  // Function
+  // Function (upload video)
+  function uploadVideo(string _videoHash, string _title) public {
+
+    videoCount++
+    videos[videoCount] = Video(videoCount, _videoHash, _title, msg.sender);
+    emit VideoUploaded(videoCount, _videoHash, _title, msg.sender);
+  }
 
 }
 
