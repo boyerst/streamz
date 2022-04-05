707a491d291771694874e5fdb7346d35fd44e340
Steve Boyer <stephen.a.boyer@gmail.com>
2022-04-04 20:46:14 -0500

    Add user account to Navbar and basic styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
+import './App.css';
 
 class Navbar extends Component {
   render() {
     return(
-
-      <p>streamzzz</p>
-
+  
+      <nav className="navbar navbar-expand-lg navbar-dark p-0 shadow text-monospace"> 
+        <a className="navbar-brand" href="#!">Streamz</a>
+        <div className="navbar-nav ms-auto">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link" href="#!">{this.props.account}</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
 
     )
   }
