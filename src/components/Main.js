41fc9f358ece84606af0fee0a0806f96e0c0ba0a
Steve Boyer <stephen.a.boyer@gmail.com>
2022-04-10 20:21:57 -0500

    Add attributes to video upload inputs

--- src/components/Main.js
+++ src/components/Main.js
@@ -10,20 +10,20 @@ class Main extends Component {
         <br></br>
         <div className="row">
           <div className="col-md-10">
-            <div className="embed-responsive-16by9" style={{ maxHeight: '768px' }}>
+            <div className="embed-responsive embed-responsive-16by9" style={{ maxHeight: '768px' }}>
               <video src="" controls></video>
             </div>
             <h3><b><i> Title </i></b></h3>
           </div>
-          <div className="col-md-2 overflow-auto text-center" style= {{ maxHeight: '768px', minWidth: '175px'}}>
+          <div className="col-md-2 overflow-visible text-center" style= {{ maxHeight: '768px', minWidth: '175px'}}>
             <h6><b> Share Video </b></h6>
             <form onSubmit={(event) => {
 
             }}>
             &nbsp;
-              <input type="file" />
+              <input type="file" accept=".mp4, .mkv, .ogg, .wmv" style={{ width: '250px' }} />
               <div className="form-group me-sm-2">
-                <input type="text" />
+                <input id="videoTitle" type="text" className="form-control-sm" placeholder="Title..." required/>
               </div>
               <button type="submit" className="btn btn-danger btn-block btn-sm">Upload!</button>
             </form>
