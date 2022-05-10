217055246bf703cd73a91f9476f56f2374e88694
Steve Boyer <stephen.a.boyer@gmail.com>
2022-05-10 14:16:34 -0500

    Add spinner with react-spinner library

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,8 @@ import NoWeb3NotificationModal from './NoWeb3NotificationModal.js'
 import Web3 from 'web3'
 import './App.css'
 import Streamz from '../abis/Streamz.json'
-import Spinner from 'react-bootstrap/Spinner';
+// import Spinner from 'react-bootstrap/Spinner';
+import HashLoader from "react-spinners/HashLoader";
 import { create } from 'ipfs-http-client'
 
 
@@ -132,10 +133,11 @@ class App extends Component {
         :
          this.state.loading 
         ?
-        <div className="loader text-center mt-5">
-          <Spinner className="mt-5" animation="grow" variant="info" style={{ width: '250px', height: '250px' }} role="status">
+        <div className="loader d-flex align-items-center justify-content-center" style={{ height: "500px"}} >
+{/*          <Spinner className="mt-5" animation="grow" variant="info" style={{ width: '250px', height: '250px' }} role="status">
             <span className="visually-hidden">Loading...</span>
-          </Spinner>
+          </Spinner>*/}
+          <HashLoader className="align-items-center" color="#6E31E0" size="150"/>
         </div>
         :
         <Main 
