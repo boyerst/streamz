c287cab9acbfd2aa13ed64b18ad0a6877c9c7a29
Steve Boyer <stephen.a.boyer@gmail.com>
2022-06-03 16:25:45 -0500

    Add testnet config code

--- truffle-config.js
+++ truffle-config.js
@@ -1,14 +1,29 @@
-``
+require('dotenv').config();
 require('babel-register');
 require('babel-polyfill');
+
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const privateKeys = process.env.PRIVATE_KEYS || ""
+
 
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: 8545,
-      network_id: "*" // Match any network id
+      network_id: "*" 
     },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(
+          privateKeys.split(','), 
+          `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`
+        )
+      },
+      gas: 5000000,
+      gasPrice: 25000000000,
+      network_id: 3
+    }
   },
   contracts_directory: './src/contracts/',
   contracts_build_directory: './src/abis/',
